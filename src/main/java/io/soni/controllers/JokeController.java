153e4dd5243d1ef6764abfc6bdb8a595e479a8db
Rahuls-95 <rahulsoni0777@gmail.com>
2021-04-17 13:46:00 +0530

    adding joke controller. closes #4

---
+++ src/main/java/io/soni/controllers/JokeController.java
@@ -0,0 +1,30 @@
+package io.soni.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.soni.services.JokeService;
+
+@Controller
+public class JokeController {
+
+	
+	private final JokeService jokeService;
+	
+
+	public JokeController(JokeService jokeService) {
+		this.jokeService = jokeService;
+	}
+	
+	
+	@RequestMapping({"/", ""})
+	public String showJoke(Model model) {
+		model.addAttribute("joke", jokeService.getJoke());
+		
+		return "index";
+		
+	}
+	
+}
